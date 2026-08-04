59213346387ccf60b628f201dbddb564d3b3fe07
Mr. 42 <admin@mr42.me>
2026-08-04 18:27:10 +0200

    Fix console app breakage by moving View parameters to di-web
    Toast interface injection was in params.php (loaded everywhere), but
    ToastInterface is only defined in di-web.php (web containers). Console
    containers tried to resolve it and failed. Move View parameter
    configuration to di-web.php so it's only applied in web containers.

--- config/params.php
+++ config/params.php
@@ -2,10 +2,7 @@
 
 declare(strict_types=1);
 
-use YiiRocks\ToastBootstrap5\ToastInjections;
-use YiiRocks\ToastBootstrap5\ToastInterface;
 use YiiRocks\ToastBootstrap5\ToastType;
-use Yiisoft\Definitions\Reference;
 
 /** @var array $params */
 
@@ -28,17 +25,5 @@ return [
         ],
         // Space-separated Bootstrap position utility classes for the toast container.
         'position' => 'top-0 end-0',
-    ],
-
-    'yiisoft/view' => [
-        'parameters' => [
-            'toast' => Reference::to(ToastInterface::class),
-        ],
-    ],
-
-    'yiisoft/yii-view-renderer' => [
-        'injections' => [
-            Reference::to(ToastInjections::class),
-        ],
     ],
 ];
